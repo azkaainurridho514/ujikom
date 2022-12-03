70f6cc5e6a183b8017434f310b25c75c7c72219f
azkaainurridho514 <azkaainurridho514@gmail.com>
2022-12-03 07:42:50 +0700

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Time extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      time: props.start
+    }
+  }
+
+  componentDidMount() {
+    this.addInterval = setInterval(() => this.increase(), 1000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.addInterval)
+  }
+
+  increase(){
+    this.setState((state) => ({
+      time: parseInt(state.time) + 1
+    }))
+  }
+
+  render(){
+    return(
+      <div>{this.state.time} - detik</div>
+    )
+  }
+
+}
+
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p><Time start="1"/></p>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
